e877a9ef26cda5b60c1a2106b2b9358e152b8514
Polina Zaprudskaya <polina.zaprudskaya@mifort.com>
2021-08-31 10:05:52 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export class Formatter {
-  public formatter(str: string | number, separator: string): string {
-    let val = Number(str).toFixed().split('.');
-    return val[0].replace(/\B(?=(\d{3})+(?!\d))/g, separator) + (val.length > 1 ? ',' + val[1] : '');
-  };
+export function formatter(str: string | number, separator: string): string {
+  let val = Number(str).toFixed().split('.');
+  return val[0].replace(/\B(?=(\d{3})+(?!\d))/g, separator) + (val.length > 1 ? ',' + val[1] : '');
 }
